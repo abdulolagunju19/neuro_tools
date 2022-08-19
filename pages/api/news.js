d79f2fc7fe21c47cd24ae6e41f3af6f5b1ebe4b2
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2022-08-19 16:30:21 -0600

    Update news.js

--- pages/api/news.js
+++ pages/api/news.js
@@ -1,5 +1,5 @@
 export default async (req, res) => {
-  const url = "https://newsdata.io/api/1/news?apikey=pub_100593ce8fd2dcd7978c6f3a7ca41b79576ac&q=neuroscience"
+  const url = ""
   const response = await fetch(url)
   const json = await response.json()
 
