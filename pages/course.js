2b043ad6d5344edfe98d236f27e665502291e477
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2022-08-26 23:07:29 -0600

    Update course.js

--- pages/course.js
+++ pages/course.js
@@ -21,22 +21,21 @@ const course = () => {
                 <Heading py={5} as='h3' size='lg'>
                 Get Started
                 </Heading>
-                <Text pb={5}>I have collected open-source resources that simulate an undergraduate neuroscience education. Over Here, find Practice Questions, YouTube videos, and other resources to support your learning.</Text>
+                <Text pb={5}>I have collected open-source resources that simulate an undergraduate neuroscience education. Over here, find Practice Questions, YouTube videos, and other resources to support your learning.</Text>
                 <Accordion allowToggle minW={700}>
                     <AccordionItem>
                         <h2>
                             <AccordionButton>
                                 <Box flex='1' textAlign='left'>
-                                    Chapter 1: Properties Of Neurons
+                                    Chapter 1: Neuron Structure and Function
                                 </Box>
                                 <AccordionIcon />
                             </AccordionButton>
                         </h2>
                         <AccordionPanel pb={4}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                            commodo consequat.
+                        <Box id="pdf1" borderWidth='5px' borderRadius={5} mt={5}>
+                            <PDFObject url="/Foundations-of-Neuroscience-1-96.pdf" width={'700px'} height={'800px'}/>
+                        </Box>
                         </AccordionPanel>
                     </AccordionItem>
 
@@ -44,16 +43,16 @@ const course = () => {
                         <h2>
                             <AccordionButton>
                                 <Box flex='1' textAlign='left'>
-                                    Chapter 2: The Action Potential
+                                    Chapter 2: Neuronal Communication
                                 </Box>
                                 <AccordionIcon />
                             </AccordionButton>
                         </h2>
                         <AccordionPanel pb={4}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                            commodo consequat.
+                        <Box id="pdf2" borderWidth='5px' borderRadius={5} mt={5}>
+                            {/* <PDFObject url="/Foundations-of-Neuroscience-97-176.pdf" width={'700px'} height={'800px'} /> */}
+                            <embed src="/Foundations-of-Neuroscience-97-176.pdf" width="700" height="800"></embed>
+                        </Box>
                         </AccordionPanel>
                     </AccordionItem>
 
@@ -61,16 +60,16 @@ const course = () => {
                         <h2>
                             <AccordionButton>
                                 <Box flex='1' textAlign='left'>
-                                    Chapter 3: Synapses
+                                    Chapter 3: Nervous System Organization
                                 </Box>
                                 <AccordionIcon />
                             </AccordionButton>
                         </h2>
                         <AccordionPanel pb={4}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                            commodo consequat.
+                        <Box id="pdf3" borderWidth='5px' borderRadius={5} mt={5}>
+                            {/* <PDFObject url="/Foundations-of-Neuroscience-177-192.pdf" width={'700px'} height={'800px'} /> */}
+                            <embed src="/Foundations-of-Neuroscience-177-192.pdf" width="700" height="800"></embed>
+                        </Box>
                         </AccordionPanel>
                     </AccordionItem>
 
@@ -78,16 +77,16 @@ const course = () => {
                         <h2>
                             <AccordionButton>
                                 <Box flex='1' textAlign='left'>
-                                    Chapter 4: The Visual System
+                                    Chapter 4: Sensory Systems
                                 </Box>
                                 <AccordionIcon />
                             </AccordionButton>
                         </h2>
                         <AccordionPanel pb={4}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                            commodo consequat.
+                        <Box id="pdf4" borderWidth='5px' borderRadius={5} mt={5}>
+                            {/* <PDFObject url="/Foundations-of-Neuroscience-193-272.pdf" width={'700px'} height={'800px'} /> */}
+                            <embed src="/Foundations-of-Neuroscience-193-272.pdf" width="700" height="800"></embed>
+                        </Box>
                         </AccordionPanel>
                     </AccordionItem>
 
@@ -95,16 +94,16 @@ const course = () => {
                         <h2>
                             <AccordionButton>
                                 <Box flex='1' textAlign='left'>
-                                    Chapter 5: The Somatosensory System
+                                    Chapter 5: Motor System
                                 </Box>
                                 <AccordionIcon />
                             </AccordionButton>
                         </h2>
                         <AccordionPanel pb={4}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                            commodo consequat.
+                        <Box id="pdf5" borderWidth='5px' borderRadius={5} mt={5}>
+                            {/* <PDFObject url="/Foundations-of-Neuroscience-273-324.pdf" width={'700px'} height={'800px'} /> */}
+                            <embed src="/Foundations-of-Neuroscience-273-324.pdf" width="700" height="800"></embed>
+                        </Box>
                         </AccordionPanel>
                     </AccordionItem>
 
@@ -112,16 +111,16 @@ const course = () => {
                         <h2>
                             <AccordionButton>
                                 <Box flex='1' textAlign='left'>
-                                    Chapter 6: The Motor System
+                                    Part 6: Behaviour
                                 </Box>
                                 <AccordionIcon />
                             </AccordionButton>
                         </h2>
                         <AccordionPanel pb={4}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                            commodo consequat.
+                        <Box id="pdf6" borderWidth='5px' borderRadius={5} mt={5}>
+                            {/* <PDFObject url="/Foundations-of-Neuroscience-325-372.pdf" width={'700px'} height={'800px'} /> */}
+                            <embed src="/Foundations-of-Neuroscience-325-372.pdf" width="700" height="800"></embed>
+                        </Box>
                         </AccordionPanel>
                     </AccordionItem>
 
@@ -129,57 +128,19 @@ const course = () => {
                         <h2>
                             <AccordionButton>
                                 <Box flex='1' textAlign='left'>
-                                    Chapter 7: Memory and Synaptic Plasticity
+                                    Part 7: Additional Materials
                                 </Box>
                                 <AccordionIcon />
                             </AccordionButton>
                         </h2>
                         <AccordionPanel pb={4}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                            commodo consequat.
-                        </AccordionPanel>
-                    </AccordionItem>
-
-                    <AccordionItem>
-                        <h2>
-                            <AccordionButton>
-                                <Box flex='1' textAlign='left'>
-                                    Chapter 8: Disorders of the Brain
-                                </Box>
-                                <AccordionIcon />
-                            </AccordionButton>
-                        </h2>
-                        <AccordionPanel pb={4}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                            commodo consequat.
-                        </AccordionPanel>
-                    </AccordionItem>
-                    <AccordionItem>
-                        <h2>
-                            <AccordionButton>
-                                <Box flex='1' textAlign='left'>
-                                    Appendix
-                                </Box>
-                                <AccordionIcon />
-                            </AccordionButton>
-                        </h2>
-                        <AccordionPanel pb={4}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                            commodo consequat.
+                        <Box id="pdf7" borderWidth='5px' borderRadius={5} mt={5}>
+                            {/* <PDFObject url="/Foundations-of-Neuroscience-373-449.pdf" width={'700px'} height={'800px'} /> */}
+                            <embed src="/Foundations-of-Neuroscience-373-449.pdf" width="700" height="800"></embed>
+                        </Box>
                         </AccordionPanel>
                     </AccordionItem>
                 </Accordion>
-                <Flex>
-                <Box borderWidth='5px' borderRadius={5} mt={5}>
-                    <PDFObject url="/exam1.pdf" width={'700px'} height={'800px'} />
-                </Box>
-                </Flex>
             </VStack>
             </Flex>
         </Container>
