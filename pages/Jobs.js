3b429abb79e2bdac2671b4627561f118153a3441
Abdul-Samad Olagunju <abdulolagunju19@gmail.com>
2023-05-16 17:03:44 -0600

    remove joboffers api call and change jobalberta to students

--- pages/Jobs.js
+++ pages/Jobs.js
@@ -11,9 +11,8 @@ import {
 
 import Container from '../components/Container'
 
-const Jobs = ({ albertaJobCenter, jobOffers }) => {
+const Jobs = ({ albertaJobCenter }) => {
     const { data } = albertaJobCenter
-    const { jobs } = jobOffers
 
     return (
     <Container>
@@ -35,37 +34,6 @@ const Jobs = ({ albertaJobCenter, jobOffers }) => {
                                 <Text p={3}>Published At: {datum.publishDate}</Text>
                             </Box>
                     ))}
-                    {jobs.map((job, index) => (
-                            <Box key={index} p={3} m={5} borderWidth='1px' borderRadius='lg' overflow='hidden' width='100%'>
-                                <Text fontWeight="bold" p={3}>{job.jobtitle}</Text>
-                                <Text fontWeight="bold" p={3}>Company: {job.company}</Text>
-                                <Image 
-                                    src='/Profile-data-bro.png'
-                                    alt='Neuroscience Image' 
-                                    boxSize='300px'
-                                    objectFit='cover'
-                                    fallbackSrc='/brain.gif'
-                                />
-                                <Text p={3}>Published At: {job.created_at}</Text>
-                                <Text p={3}>City: {job.city}</Text>
-                                <Text p={3}>Number of Applicants: {job.applicants}</Text>
-                                <Accordion allowToggle mt={3}>
-                                        <AccordionItem>
-                                            <h2>
-                                                <AccordionButton>
-                                                    <Box flex='1' textAlign='left'>
-                                                        Description
-                                                    </Box>
-                                                    <AccordionIcon />
-                                                </AccordionButton>
-                                            </h2>
-                                            <AccordionPanel pb={4}>
-                                                {job.fulldescription}
-                                            </AccordionPanel>
-                                        </AccordionItem>
-                                    </Accordion>
-                            </Box>
-                    ))} 
                 </div>
                 </Flex>
             </Flex>
@@ -74,22 +42,14 @@ const Jobs = ({ albertaJobCenter, jobOffers }) => {
 }
 
 export async function getServerSideProps () {
+    const [ albertaJobCenterRes ] = await Promise.all([
+        fetch("https://www.albertajobcentre.ca/api/v1.1/public/jobs?page=1&q=student")
+    ]);
+    const [ albertaJobCenter ] = await Promise.all([
+    albertaJobCenterRes.json()
+    ]);
 
-    try{
-        const [albertaJobCenterRes, jobOffersRes] = await Promise.all([
-        fetch("https://www.albertajobcentre.ca/api/v1.1/public/jobs?page=1&q=scientist"), 
-        fetch("https://careers.joboffer.ca/api/search?page=0&city=alberta")
-      ]);
-      const [albertaJobCenter, jobOffers] = await Promise.all([
-        albertaJobCenterRes.json(), 
-        jobOffersRes.json()
-      ]);
-    } catch (error){
-        console.error('Error fetching data:', error);
-    }
-    
-
-      return { props: { albertaJobCenter, jobOffers } };
+    return { props: { albertaJobCenter } };
 }
 
 export default Jobs
