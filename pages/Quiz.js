0d6764ba2e676c5cc1a93b9674c3e09f8f67ee00
Abdul-Samad Olagunju <abdulolagunju19@gmail.com>
2023-05-17 15:08:00 -0600

    add toast to quiz components to check correct answer

--- pages/Quiz.js
+++ pages/Quiz.js
@@ -16,7 +16,8 @@ import {
     Button, 
     Spacer, 
     HStack,
-    Center
+    Center,
+    useToast
 } from '@chakra-ui/react'
 import { CloseIcon } from '@chakra-ui/icons'
 
@@ -31,6 +32,7 @@ const Quiz = ({ questions }) => {
     const [id, setID] = useState('')
 
     const router = useRouter();
+    const toast = useToast();
 
     const handleSubmit = async (e) => {
         const { data, error } = await supabase
@@ -50,38 +52,85 @@ const Quiz = ({ questions }) => {
         router.reload()
     }
 
+    const handleMultipleChoice = async (choice, quizId) => {
+        const { data, error } = await supabase
+            .from('questions')
+            .select('correct_answer')
+            .eq('id', quizId)
+        
+        if(choice === data[0].correct_answer){
+            toast({
+                title: 'Correct!',
+                status: 'success',
+                duration: 1000,
+                isClosable: true,
+              })
+        } else{
+            toast({
+                title: 'Incorrect!',
+                status: 'success',
+                duration: 1000,
+                isClosable: true,
+              })
+        }
+    }
+
     return (
         <Container>
             <Flex maxW={700} m={5} flexDir="column" justifyContent="center">
-                <VStack justifyContent="center">
+                <VStack justifyContent="center">       
+                <Heading>Multiple Choice Test</Heading>             
+                    <Box>
+                        {
+                            (questions).map((question) =>
+                                <>
+                                    <Center key={question.id}>
+                                        <Box p={3} m={5} borderWidth='1px' borderRadius='lg' overflow='hidden' width='100%'>
+                                            <Text fontSize='xl' fontWeight="bold" as='ins'>Question {question.id}</Text>
+                                            <Text fontSize='xl' fontWeight="bold">{question.question}</Text>
+                                            <Flex m={5} direction="column" alignItems="start">
+                                                    <Button onClick={() => handleMultipleChoice(question.choiceA, question.id)} variant='ghost'>A: {question.choiceA}</Button>
+                                                    <Button onClick={() => handleMultipleChoice(question.choiceB, question.id)} variant='ghost'>B: {question.choiceB}</Button>
+                                                    <Button onClick={() => handleMultipleChoice(question.choiceC, question.id)} variant='ghost'>C: {question.choiceC}</Button>
+                                                    <Button onClick={() => handleMultipleChoice(question.choiceD, question.id)} variant='ghost'>D: {question.choiceD}</Button>
+                                            </Flex>
+                                            <HStack>
+                                                <Spacer />
+                                                <Button align="right" type="submit" onClick={() => handleDelete(question.id)} rightIcon={<CloseIcon />} >Delete</Button>
+                                            </HStack>
+                                        </Box>
+                                    </Center>
+                                </>
+                            )
+                        }
+                    </Box>
                     <Heading pt={5} pb={3} as='h3' size='lg'>
                         Add Questions to Help Other Students!
                     </Heading>
                     <Box p={3} m={5} borderWidth='1px' borderRadius='lg' overflow='hidden' w='100%'>
                         <FormControl>
-                            <FormLabel>Add a Question!</FormLabel>
-                            <FormLabel>Your Name</FormLabel>
+                            <FormLabel>Name</FormLabel>
                             <Input 
                                 placeholder="Name" 
                                 value={name}
                                 onChange={(e) => setName(e.target.value)}
                                 variant='filled'
                             />
-                            <FormLabel pt={3}>Your Email</FormLabel>
+                            <FormLabel pt={3}>Email</FormLabel>
                             <Input 
                                 placeholder="Email" 
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                                 variant='filled'
                             />
-                            <FormLabel pt={3}>Your Question</FormLabel>
+                            <FormLabel pt={3}>Question</FormLabel>
                             <Input 
                                 placeholder="Question"
                                 value={question}
                                 onChange={(e) => setQuestion(e.target.value)}
                                 variant='filled'
                             />
-                            <FormLabel pt={3}>Your Answer</FormLabel>
+                            <FormLabel pt={3}>Answer</FormLabel>
                             <Input 
                                 placeholder="Answer"
                                 value={answer}
@@ -97,54 +146,6 @@ const Quiz = ({ questions }) => {
                             </Button>
                         </FormControl>
                     </Box>
-                    <Box p={3} m={5} borderWidth='1px' borderRadius='lg' overflow='hidden' w='100%'>
-                        <FormControl pt={3}>
-                            <FormLabel>Remove a Question!</FormLabel>
-                            <FormLabel>The Question ID: </FormLabel>
-                            <Input
-                                placeholder="ID" 
-                                value={id}
-                                onChange={(e) => setID(e.target.value)}
-                                variant='filled'
-                            />
-                            <Button 
-                                mt={3} 
-                                type="submit"
-                                onClick={handleDelete}
-                            >
-                                Delete
-                            </Button>
-                        </FormControl>
-                    </Box>
-                    <Heading pt={5} pb={3} as='h3' size='lg'>
-                        Questions:
-                    </Heading>
-                    <Flex>
-                    <Box>
-                        {
-                            (questions).map((question) =>
-                                <>
-                                    <Center>
-                                        <Box key={question.id} p={3} m={5} borderWidth='1px' borderRadius='lg' overflow='hidden' width='100%'>
-                                            <Text fontSize='xl' fontWeight="bold" as='ins'>Question {question.id}</Text>
-                                            <Text fontSize='xl' fontWeight="bold">{question.question}</Text>
-                                            <Flex m={5} direction="column" alignItems="start">
-                                                    <Button variant='ghost'>A: {question.choiceA}</Button>
-                                                    <Button variant='ghost'>B: {question.choiceB}</Button>
-                                                    <Button variant='ghost'>C: {question.choiceC}</Button>
-                                                    <Button variant='ghost'>D: {question.choiceD}</Button>
-                                            </Flex>
-                                            <HStack key={question.id}>
-                                                <Spacer />
-                                                <Button align="right" type="submit" onClick={() => handleDelete(question.id)} rightIcon={<CloseIcon />} >Delete</Button>
-                                            </HStack>
-                                        </Box>
-                                    </Center>
-                                </>
-                            )
-                        }
-                    </Box>
-                    </Flex>
                 </VStack>
             </Flex>
         </Container>
